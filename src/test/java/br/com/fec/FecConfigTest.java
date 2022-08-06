8bd85fefb21ed72040584abda86af1cc574d03d3
Adriano Avelino <adriano-st777@hotmail.com>
2022-08-06 16:13:19 -0300

    (test) updates tests to config connector

--- src/test/java/br/com/fec/FecConfigTest.java
+++ src/test/java/br/com/fec/FecConfigTest.java
@@ -10,21 +10,41 @@ import java.util.Map;
 
 class FecConfigTest {
     @Test
-    @DisplayName("Should have default value for non required property")
-    void ShouldHaveDefaultValueForNonRequiredProperty() {
+    @DisplayName("Should have default value for non required message value property")
+    void ShouldHaveDefaultValueForNonRequiredMessageValueProperty() {
         Map<String, String> props = new HashMap<>();
         FecConfig config = new FecConfig(props);
         String defaultValue = "#{Name.full_name}";
-        Assertions.assertEquals(defaultValue, config.getString(FecConfig.MESSAGE_VALUE));
+        String propertyValue = config.getString(FecConfig.MESSAGE_VALUE);
+        Assertions.assertEquals(defaultValue, propertyValue);
     }
 
     @Test
-    @DisplayName("Should define the Value for non required property")
-    void shouldDefineTheValueForNonRequiredProperty() {
+    @DisplayName("Should have default value for non required topic name property")
+    void ShouldHaveDefaultValueForNonRequiredProperty() {
+        Map<String, String> props = new HashMap<>();
+        FecConfig config = new FecConfig(props);
+        String defaultValue = "topicName";
+        String propertyValue = config.getString(FecConfig.TOPIC_NAME);
+        Assertions.assertEquals(defaultValue, propertyValue);
+    }
+
+    @Test
+    @DisplayName("Should define the Value for non required message value property")
+    void shouldDefineTheValueForNonRequiredMessageValueProperty() {
         Map<String, String> props = new HashMap<>();
         props.put(FecConfig.MESSAGE_VALUE, "#{PhoneNumber.phoneNumber}");
         FecConfig config = new FecConfig(props);
         Assertions.assertEquals("#{PhoneNumber.phoneNumber}", config.getString(FecConfig.MESSAGE_VALUE));
+    }
+
+    @Test
+    @DisplayName("Should define the Value for non required topic name property")
+    void shouldDefineTheValueForNonRequiredTopicNameProperty() {
+        Map<String, String> props = new HashMap<>();
+        props.put(FecConfig.TOPIC_NAME, "topic");
+        FecConfig config = new FecConfig(props);
+        Assertions.assertEquals("topic", config.getString(FecConfig.TOPIC_NAME));
     }
 
 }
